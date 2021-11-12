b9cd1922f6d53a04cb079e962e7aabad5d454bfe
Anest <32736719+AnestLarry@users.noreply.github.com>
2021-11-12 09:49:00 -0400

    feat(https): add https supporting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::borrow::Borrow;
 use std::env::args as std_args;
+use std::fs::File;
+use std::io::BufReader;
 
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use rustls::internal::pemfile::{certs, pkcs8_private_keys};
+use rustls::{NoClientAuth,ServerConfig};
 use serde::Serialize;
 
 mod data_generator;
@@ -309,6 +313,7 @@ async fn main() -> std::io::Result<()> {
     let args: Vec<String> = std_args().collect();
     let mut ip: String = "127.0.0.1".into();
     let mut port = 8000u16;
+    let (mut crt_file, mut key_file) = ("", "");
     let mut i = 1;
     // argv parse
     while i < args.len() {
@@ -320,6 +325,11 @@ async fn main() -> std::io::Result<()> {
             "port" => {
                 port = args.get(i + 1).unwrap().clone().parse().unwrap();
                 i += 2;
+            }
+            "https" => {
+                crt_file = args.get(i + 1).unwrap();
+                key_file = args.get(i + 2).unwrap();
+                i += 3;
             }
             _ => {
                 if cfg!(debug_assertions) {
@@ -333,7 +343,7 @@ async fn main() -> std::io::Result<()> {
     }
     println!("listen on:{0}:{1}", ip, port);
     drop(i);
-    HttpServer::new(
+    let server = HttpServer::new(
         || App::new()
             .service(
                 all_methods_v1::gets_v1::get_score()
@@ -344,9 +354,23 @@ async fn main() -> std::io::Result<()> {
             .default_service(
                 web::route().to(method_not_found)
             )
-    )
-        .bind(format!("{0}:{1}", ip, port))?
-        .run()
-        .await
+    );
+    if crt_file == "" {
+        server.bind(format!("{0}:{1}", ip, port))?
+            .run()
+            .await
+    }
+    else {
+        let mut config = ServerConfig::new(NoClientAuth::new());
+        let cert_file = &mut BufReader::new(File::open(crt_file).unwrap());
+        let key_file = &mut BufReader::new(File::open(key_file).unwrap());
+        let cert_chain = certs(cert_file).unwrap();
+        let mut keys = pkcs8_private_keys(key_file).unwrap();
+        config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+        server.bind_rustls(format!("{0}:{1}", ip, port),config)?
+            .run()
+            .await
+
+    }
 }
 
